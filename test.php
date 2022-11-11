952d994173a98c22322dfdf94cd2a1f36d583d98
sumitkumary8 <114694804+sumitkumary8@users.noreply.github.com>
2022-11-12 02:21:10 +0530

    Update test.php

--- test.php
+++ test.php
@@ -1,4 +1,11 @@
-<?php
-echo test;
-
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+asdasdas asd asd asd
+</body>
+</html>
